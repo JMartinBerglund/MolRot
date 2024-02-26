dedc3604768ae43b3ac214c999d462fae638b541
JMartinBerglund <161346739+JMartinBerglund@users.noreply.github.com>
2024-02-26 19:53:51 +0100

    Add files via upload

---
+++ Utility/parameters.py
@@ -0,0 +1,9 @@
+"""
+Module containing parameters used by the library
+"""
+import math
+
+eps0 = (4.*math.pi)**(-1.) # vacuum permittivitiy (atomic units)
+c     = 137.035999084 # speed of light (atomic units), ref: wikipedia
+fs2au = 41.341373 # converting femtoseconds to atomic time
+
