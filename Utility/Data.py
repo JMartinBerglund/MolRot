f9b4d2438f34c8d8b0cd1f8a05579f522190f763
Martin Berglund <161346739+JMartinBerglund@users.noreply.github.com>
2025-03-12 21:41:33 +0100

    Add files via upload

--- Utility/Data.py
+++ Utility/Data.py
@@ -1,3 +1,16 @@
 """
-This is the data module
+This module contains method for I/O-data for the tomography scheme
 """
+
+def write_data(obj, filename):
+    """Write data about a tomography scheme to file
+        Args:
+            
+            obj:
+                object to write out
+
+            filename: string
+                Name of file to write out
+    """
+    import pandas as pd
+    print(filename)
--- Utility/parameters.py
+++ Utility/parameters.py
@@ -15,3 +15,90 @@ atime2s   = 2.4188843e-17 # atomic ime to second
 
 I0_SI     = Hartree2J / (Bohr2m**2. * atime2s) # I0 to SI units 
 
+
+"""
+Systems of molecular ions
+"""
+def set_MolParams(Molecule):
+    """
+    Retruns a dictionary of molecular paramters
+    """
+    if Molecule == 'MgH+':
+        Mol = {'B': 2.88*10**(-5.), 'Da': 16.20, 'D': 1.18, 'Mol': 'MgH+'}
+    elif Molecule == 'CaH+':
+        Mol  = {'B': 2.15*10**(-5.), 'Da': 16.59, 'D': 2.38, 'Mol': 'CaH+'}
+    elif Molecule == 'CaD+':
+        Mol  = {'B': 1.10*10**(-5.), 'Da': 16.59, 'D': 2.38, 'Mol': 'CaD+'}
+    else:
+        print("Warning molecular system {} not found in list, returning empty dictionary".format(Molecule))
+        Mol = {}
+
+    return Mol
+
+"""
+Combinations of molecular parameters and electric field
+"""
+
+def get_chiD(Molecule: float, eps0: float) -> float:
+    """
+    Returns the parameters chi_D from rotational constant, dipole moment and the electric field
+    """
+    chiD = Molecule['D'] * eps0 * Molecule['B']**(-1.)
+    return chiD
+
+def get_eps0(Molecule: dict, chiD: float) -> float:
+    """
+    Returns the electric field strength from rotational constant, dipole moment and the parameters chiD
+    """
+    eps0 = Molecule['B'] * chiD * Molecule['D']**(-1.)
+    return eps0
+
+
+"""
+Printing information of the molecular parameters
+"""
+
+def print_molecular_info(Molecule):
+    """
+    Prints out what molecular system we are using
+    """
+    try:
+        print("Molecular ion used for the optimization: {}".format(Molecule['Mol']))
+    except Molecule['Mol'] == None:
+        print("No molecular ion system provided")
+
+
+def print_Brot(Molecule):
+    """
+    Prints out the rotational constant of the molecule
+    """
+    try:
+        print("Rotational constant used for the optimization: {} (atomic units)".format(Molecule['B']))
+    except Molecule == None:
+        print("No molecular ion system provided")
+    except Molecule['B'] not in Molecule:
+        print("No rotational constant provided")
+
+def print_Dalpha(Molecule):
+    """
+    Prints out the polarizability anisotropy of the molecule
+    """
+    try:
+        print("Polarizability anisotropy used for the optimization: {} (atomic units)".format(Molecule['Da']))
+    except Molecule == None:
+        print("No molecular ion system provided")
+    except Molecule['Da'] not in Molecule:
+        print("No polarizability anisotropy provided")
+
+def print_dip(Molecule):
+    """
+    Prints out the dipole moment of the molecule
+    """
+    try:
+        print("Dipole moment used for the optimization: {} (atomic units)".format(Molecule['D']))
+    except Molecule == None:
+        print("No molecular ion system provided")
+    except Molecule['D'] not in Molecule:
+        print("No dipole moment provided")
+
+
