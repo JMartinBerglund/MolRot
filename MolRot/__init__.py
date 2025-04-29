324bb4ac7483f498596fb65e1c9d0fb69b7247db
Martin Berglund <161346739+JMartinBerglund@users.noreply.github.com>
2025-04-29 21:24:14 +0200

    Add files via upload

--- MolRot/__init__.py
+++ MolRot/__init__.py
@@ -10,18 +10,20 @@ sys.path.append("/home/martin/Work/GitHub/MolRot/Tomography/")
 sys.path.append("/home/martin/Work/GitHub/MolRot/Interferometry/")
 sys.path.append("/home/martin/Work/GitHub/MolRot/Utility/")
 sys.path.append("/home/martin/Work/GitHub/MolRot/StateCreation/")
+sys.path.append("/home/martin/Work/GitHub/MolRot/Vibrations/")
 import Interferometry
 import Data
 import Opt
 import Utility
 import Tomography
 import StateCreation
+import Vibrations
 
 print("This is MolRot")
 print("Running on python version:")
 print(sys.version)
 print(sys.version_info)
 
-__all__ = ["Tomography", "Interferometry", "Data", "Opt", "Utility", "StateCreation"]
+__all__ = ["Tomography", "Interferometry", "Data", "Opt", "Utility", "StateCreation", "Vibration"]
 
 
